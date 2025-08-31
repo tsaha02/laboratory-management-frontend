5d84f1bb8644578df6ca655046af07a2adeef7f3
Tanmoy Saha <tanmoy@solivatech.com>
2025-08-31 13:40:38 +0530

    dockerfile added

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,11 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+// eslint.config.js
+const tseslint = require('typescript-eslint');
+const prettierConfig = require('eslint-config-prettier');
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+module.exports = tseslint.config(
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
+    ignores: ['dist/'], // Ignore the build output directory
   },
-])
+  ...tseslint.configs.recommended,
+  prettierConfig // This disables ESLint rules that conflict with Prettier
+);
